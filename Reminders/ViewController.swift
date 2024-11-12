66a8e2276792581f200fed810add1daaca5bac54
talehMammadov <taleh.mammadov99@gmail.com>
2024-11-12 21:14:30 +0400

    Initial Commit

---
+++ Reminders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminders
+//
+//  Created by Taleh on 12.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
